be0790b0377dd6425097201534f688155f361e05
Joy <joy.logs@gmail.com>
2024-01-09 22:21:43 +0530

    Adding initial Screen

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Joy Banerjee on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
