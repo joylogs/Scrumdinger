8798b1796813e78c3c07208d63dc5ee10460d374
Joy <joy.logs@gmail.com>
2024-01-10 23:08:41 +0530

    Added DailyScrum Model

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,17 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Joy Banerjee on 10/01/24.
+//
+
+import Foundation
+
+
+struct DailyScrum {
+    
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
