e164e8bac43ac938bc319d07bf8a52d418d9b0c1
Joy <joy.logs@gmail.com>
2024-02-14 23:52:28 +0530

    added CardView

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,22 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Joy Banerjee on 14/02/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
