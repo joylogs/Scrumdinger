9fc2c5e091ca47933990fd3655cbac2ded07c60d
Joy <joy.logs@gmail.com>
2024-02-15 16:54:11 +0530

    completing the CardView

--- Scrumdinger/LabelStyle.swift
+++ Scrumdinger/LabelStyle.swift
@@ -5,6 +5,19 @@
 //  Created by Joy Banerjee on 15/02/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {            
+            configuration.title
+            configuration.icon
+        }
+    }
+}
 
 
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
