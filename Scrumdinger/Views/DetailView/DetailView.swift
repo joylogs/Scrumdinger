2508430b0365f904d3c30d8985fc45d3ff6fd0b8
Joy <joy.logs@gmail.com>
2024-02-21 23:46:43 +0530

    added Bindings to DetailView

--- Scrumdinger/Views/DetailView/DetailView.swift
+++ Scrumdinger/Views/DetailView/DetailView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct DetailView: View {
     
-    let scrum: DailyScrum
+    @Binding var scrum: DailyScrum
     
+    @State private var editingScrum = DailyScrum.emptyScrum
     @State private var isPresentingEditView = false
     
     var body: some View {
@@ -74,7 +75,7 @@ struct DetailView: View {
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationStack {
-            DetailView(scrum: DailyScrum.sampleData[0])
+            DetailView(scrum: .constant(DailyScrum.sampleData[0]))
         }
     }
 }
