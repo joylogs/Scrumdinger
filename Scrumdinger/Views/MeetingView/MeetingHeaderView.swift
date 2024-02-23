de681a32054d23426615db1ec382584aa3477c93
Joy <joy.logs@gmail.com>
2024-02-23 23:49:13 +0530

    adding secondsElapsed, secondsRemaining

--- Scrumdinger/Views/MeetingView/MeetingHeaderView.swift
+++ Scrumdinger/Views/MeetingView/MeetingHeaderView.swift
@@ -8,13 +8,41 @@
 import SwiftUI
 
 struct MeetingHeaderView: View {
+    
+    let secondsElapsed: Int
+    let secondsRemaining: Int
+    
+    private var totalSeconds: Int {
+        secondsElapsed + secondsRemaining
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("\(secondsElapsed)", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("\(secondsRemaining)", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel("Time remaining")
+        .accessibilityValue("10 minutes")
     }
 }
 
 struct MeetingHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingHeaderView()
+        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180)
+            .previewLayout(.sizeThatFits)
     }
 }
