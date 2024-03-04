dfafeb9a6e519f7895181585161efc8a97ba1556
Joy <joy.logs@gmail.com>
2024-03-04 23:59:47 +0530

    added prop in MeetingFooterView

--- Scrumdinger/Views/MeetingView/MeetingFooterView.swift
+++ Scrumdinger/Views/MeetingView/MeetingFooterView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct MeetingFooterView: View {
+    let speakers: [ScrumTimer.Speaker]
+    
+    private var speakerNumber: Int? {
+        guard let index = speakers.firstIndex(where: { !$0.isCompleted }) else { return nil }
+        return index + 1
+    }
+    
     var body: some View {
         HStack {
             Text("Speaker 1 of 3")
@@ -22,7 +29,7 @@ struct MeetingFooterView: View {
 
 struct MeetingFooterView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingFooterView()
+        MeetingFooterView(speakers: DailyScrum.sampleData[0].attendees.speakers)
             .previewLayout(.sizeThatFits)
     }
 }
