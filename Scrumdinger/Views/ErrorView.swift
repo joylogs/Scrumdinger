85b2489993ab2651fdd9a48e7ebd18aaaee6dfeb
Joy <joy.logs@gmail.com>
2024-03-18 23:59:07 +0530

    Adding ErrorView, ErrorWrapper

---
+++ Scrumdinger/Views/ErrorView.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorView.swift
+//  Scrumdinger
+//
+//  Created by Joy Banerjee on 18/03/24.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ErrorView()
+    }
+}
